c49df5a0ec599dec16def27ddadf3161f1844f03
ChuYong <jakgon@naver.com>
2023-06-09 20:20:56 +0900

    feat: 코드가 어지러워요

--- layout/DocumentLayout.tsx
+++ layout/DocumentLayout.tsx
@@ -1,13 +1,18 @@
 import {FaChevronLeft} from "react-icons/fa";
-import webViewJavaScriptBridge from 'webview-javascript-bridge';
+import {useEffect} from "react";
 
-async function returnPage() {
-    await webViewJavaScriptBridge.sendMessage({
-        action: 'return',
-    });
-}
 
 export default function DocumentLayout({children, title}: {children: React.ReactNode, title: String}) {
+    let returnPage: () => void = () => {};
+    useEffect(() => {
+        import('webview-javascript-bridge').then((ir) => {
+            returnPage = () => {
+                ir.default.sendMessage({
+                    action: 'return',
+                });
+            }
+        })
+    }, []);
     return <div>
         <div style={{paddingLeft: 3, paddingBottom: 16, paddingTop: 16}}>
             <div style={{
