9e9aa538009cb3e4bcac62d4285d89a2ff6416d9
ChuYong <jakgon@naver.com>
2023-06-23 00:24:20 +0900

    feat: 있어보이는 공지사항 추가

--- pages/notice.tsx
+++ pages/notice.tsx
@@ -1,7 +1,25 @@
 import DocumentLayout from "@/layout/DocumentLayout";
+import {FaChevronRight} from "react-icons/fa";
 
 export default function Notice() {
     return <DocumentLayout title={"공지사항"}>
-        여기에 공지사항이 들어갑니다
+        <NoticeElements title={"나쁜말 카운터 런칭!"} date={"2023.06.23."}></NoticeElements>
     </DocumentLayout>
+}
+
+function NoticeElements({title, date}) {
+    return <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: "space-between"
+    }}>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            <a style={{fontSize: '24'}}>{title}</a>
+            <a>{date}</a>
+        </div>
+        <FaChevronRight/>
+    </div>
 }
